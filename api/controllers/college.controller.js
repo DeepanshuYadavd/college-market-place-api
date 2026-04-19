5fdbecccdbd460e59c9d81ac6f9997616dc2eebe
DeepanshuYadavd <dipanshuy591@gmail.com>
2026-04-19 16:32:19 +0530

    updated

--- api/controllers/college.controller.js
+++ api/controllers/college.controller.js
@@ -11,22 +11,18 @@ export const createCollge = async (req, res, next) => {
     }
     // case : 1 only one admin create one college
     const isAdminExist = await College.findOne({ admin: req.user.id });
-
     if (isAdminExist) {
       return res.status(400).json({
         message: "Admin can create only one college",
       });
     }
-
     //  case 2 : two admin can not create one (same) college
     const isCollegeExist = await College.findOne({ collegeName: collegeName });
-
     if (isCollegeExist) {
       return res.status(400).json({
         message: "College is already exist",
       });
     }
-
     const college = await College.create({
       collegeName,
       address,
